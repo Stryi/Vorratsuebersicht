e2c682de9450dac63213dc1eb089fa6608dad43a
Christian <cstryi@gmx.de>
2020-01-10 19:52:25 +0100

    Google Play Version 4.12 (CodeVersion 57)

--- Activities/MainActivity.cs
+++ Activities/MainActivity.cs
@@ -63,7 +63,7 @@ namespace VorratsUebersicht
             // Damit Pre-Launch von Google Play Store nicht immer wieder
             // in die EAN Scan "Falle" tappt und da nicht wieder rauskommt.
             // (meistens nächster Tag)
-            MainActivity.preLaunchTestEndDay = new DateTime(2020, 01, 07);
+            MainActivity.preLaunchTestEndDay = new DateTime(2020, 01, 11);
 
             base.OnCreate(bundle);
 
