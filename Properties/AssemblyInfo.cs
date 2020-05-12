61defddf29c00f830e6460afb113416d5750cb68
Christian <cstryi@gmx.de>
2020-05-12 21:46:56 +0200

    Google Play Version 4.52 (Code Version 80)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using Android.App;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.51")]
-[assembly: AssemblyFileVersion("4.51.0.0")]
+[assembly: AssemblyVersion("4.52")]
+[assembly: AssemblyFileVersion("4.52.0.0")]
 
