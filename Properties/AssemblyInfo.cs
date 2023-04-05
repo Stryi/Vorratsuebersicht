cfd1efe38994c63f9849220b57d388382bf468c1
Christian Stryi <christian.stryi@audius.de>
2023-04-05 06:39:01 +0200

    C201 - Versionsnummer auf 7.15 gesetzt.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using Android.App;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("7.14")]
-[assembly: AssemblyFileVersion("7.14.0.0")]
+[assembly: AssemblyVersion("7.15")]
+[assembly: AssemblyFileVersion("7.15.0.0")]
 [assembly: InternalsVisibleTo("VueUnitTestProject")]
 
