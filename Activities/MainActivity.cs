4677efda6232d3024a3eb53e80917517f0b629f3
Christian Stryi <cstryi@freenet.de>
2023-04-05 20:41:50 +0200

    Google Play Version 7.15 (Code Version 132)

--- Activities/MainActivity.cs
+++ Activities/MainActivity.cs
@@ -40,7 +40,7 @@ namespace VorratsUebersicht
             // Damit Pre-Launch von Google Play Store nicht immer wieder
             // in die EAN Scan "Falle" tappt und da nicht wieder rauskommt.
             // (meistens nächster Tag)
-            MainActivity.preLaunchTestEndDay = new DateTime(2023, 04, 02);
+            MainActivity.preLaunchTestEndDay = new DateTime(2023, 04, 06);
 
             // Zusammen mit minSdkVersion="19" verhindert das den Fehler: Android.Content.Res.Resources+NotFoundException: 'File res/drawable/abc_vector_test.xml from drawable resource ID
             //AppCompatDelegate.CompatVectorFromResourcesEnabled = true;
